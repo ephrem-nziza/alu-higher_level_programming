3e43a53a9e788213e009e36ab0d03f77d78ceebc
ephrem-nziza <e.nziza@alustudent.com>
2026-06-23 04:52:58 -0700

    Fixing Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-s = "Python is object-oriented programming with Python"
-print(s[10:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
