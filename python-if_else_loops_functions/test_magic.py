5dd30dc48d2cb5d90365e58e032e4001c15df45b
ephrem-nziza <e.nziza@alustudent.com>
2026-06-25 04:35:00 -0700

    Magic calculation function from bytecode

---
+++ python-if_else_loops_functions/test_magic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102_magic_calculation').magic_calculation
+
+print(magic_calculation(1, 2, 3))
+print(magic_calculation(5, 2, 3))
+print(magic_calculation(5, 2, 1))
