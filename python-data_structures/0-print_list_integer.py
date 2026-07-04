038b3db31619625178d509ebcffff2b9b26a4e4c
ephrem-nziza <e.nziza@alustudent.com>
2026-07-04 03:26:45 -0700

    Fixing my codes and PEP8 Validation

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -5,4 +5,4 @@
 def print_list_integer(my_list=[]):
     """Print all integers of a list, one per line."""
     for number in my_list:
-        print("{}".format(number))
+        print("{:d}".format(number))
